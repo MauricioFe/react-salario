38c2d2ac4fa04e55c0291170fd56a8d09cb5df7a
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-17 06:57:09 -0300

    Organização da estrutura

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
-import ProjetoBase from './components/ProjetoBase/ProjetoBase';
+
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+       baseINSS:0,
+       discountINSS:0,
+       baseIRPF:0,
+       discountIRPF:0,
+       net:0,
+       baseINSS:0,
+    }
+}
   render() {
-    return <ProjetoBase />;
+    const {fullSalary} = this.state;
+    return (
+      <div className="container">
+        <h1 className="center">React Salário</h1>
+
+      
+      </div>
+    );
   }
 }
